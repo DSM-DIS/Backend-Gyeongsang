ac98096f43082ff00208c27bf1601d0c0d7265af
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-09-15 20:24:23 +0900

    [CREATE] Swagger Setting

--- src/main/java/com/dsm/gyeongsang/controller/ParticipantController.java
+++ src/main/java/com/dsm/gyeongsang/controller/ParticipantController.java
@@ -9,6 +9,7 @@ import com.dsm.gyeongsang.domains.repository.ParticipantRepository;
 import com.dsm.gyeongsang.domains.repository.UserRepository;
 import com.dsm.gyeongsang.utils.exception.CodeMismatchException;
 import com.dsm.gyeongsang.utils.exception.IdMismatchException;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/diary")
+@Api(value = "Participant Controller")
 public class ParticipantController {
 
     private ParticipantRepository participantRepository;
@@ -32,8 +34,15 @@ public class ParticipantController {
         this.userRepository = userRepository;
     }
 
+    @ApiOperation(value = "일기장 참여", notes = "participation_personnel_list 테이블에 code, User 매칭")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "정상적인 응답"),
+            @ApiResponse(code = 404, message = "아이디 매칭 실패 또는 코드 매칭 실패"),
+            @ApiResponse(code = 500, message = "500")
+    })
     @PostMapping("/diary-participation")
-    public void participate(@RequestParam("id") String id, @RequestParam("code") String code) {
+    public void participate(@ApiParam(value = "ididididid", required = true) @RequestParam("id") String id,
+                            @ApiParam(value = "030816", required = true) @RequestParam("code") String code) {
         DiaryBook diaryBook = diaryBookRepository.findByCode(code)
                 .orElseThrow(() -> new CodeMismatchException());
         User user = userRepository.findById(id)
